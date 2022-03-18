cda76219b9a96dad450949a9ac9822cb3667903b
ruchi0909 <88983102+ruchi0909@users.noreply.github.com>
2022-03-18 21:49:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+console.log(09)
+console.log(6.5)
+console.log(true)
+const pi = 3.14
+var dia = 6
+let rad = dia/2
+let AOC = pi*rad*rad
+let COC = 2*pi*rad
+console.log("Radius of the circle = "+rad)
+console.log("Area of a circle = "+AOC)
+console.log("Circumference of a circle = "+COC)
